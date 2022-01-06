ff13ec5f3f1933f03ef3ad821c1d00ed575fdba0
Kevin Tran <kevin.tran.kt@gmail.com>
2022-01-06 04:26:07 -0800

    ci

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ client.on('ready', () => {
 client.on('messageCreate', message => {
     if (message.content == 'ping') {
         message.reply({
-            content: 'pong! 1234'
+            content: 'pong! yeet'
         })
     }
 })
