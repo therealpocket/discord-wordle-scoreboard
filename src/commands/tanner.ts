5c114b59c62d91cc7d06483db6f06d2680fb5909
pocket <me@pocket.rip>
2022-01-28 12:39:00 -0800

    Create tanner.ts

---
+++ src/commands/tanner.ts
@@ -0,0 +1,31 @@
+import { ICommand } from "wokcommands";
+import wordleScoreboard, { generateScoreboardEmbed } from "../features/wordle-scoreboard";
+import wordleScoreboardSchema from "../models/wordle-scoreboard-schema";
+
+
+export default {
+    category: 'Scoreboard',
+    description: 'Just praise tanner',
+    slash: true,
+    callback: async ({ client, guild, interaction }) => {
+        await interaction.deferReply();
+
+        if (!guild) {
+            return interaction.reply({
+                content: `Please use this command within a server`,
+                ephemeral: true
+            })
+        }
+
+        const guildScores = await wordleScoreboardSchema.find({
+            guildId: guild.id
+        })
+
+        const guildScoresEmbed = generateScoreboardEmbed(client, guildScores, 'SCOREBOARD');
+
+        await interaction.editReply({
+            content: `👑 BOW DOWN TO ${client.users.cache.get('329439633647730691')} NOT BECAUSE HE HAS THE HIGHEST CORE BUT BECAUSE HE'S JUST A GOOD GUY 👑`
+        });
+
+    }
+} as ICommand
